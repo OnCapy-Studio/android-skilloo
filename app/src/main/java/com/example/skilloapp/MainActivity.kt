ec5c9b8cf73464d0d45885288c3e809bc27da096
myrellauchoa <myrellauchoast@gmail.com>
2023-05-10 19:41:44 -0300

    feat: first login scope

--- app/src/main/java/com/example/skilloapp/MainActivity.kt
+++ app/src/main/java/com/example/skilloapp/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.skilloapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var buttonLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextUsername = findViewById(R.id.editTextUsername)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        buttonLogin = findViewById(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (isValidCredentials(username, password)) {
+                showToast("Login successful!")
+            } else {
+                showToast("Invalid credentials!")
+            }
+        }
+    }
+
+    private fun isValidCredentials(username: String, password: String): Boolean {
+        // Aqui você pode adicionar a lógica para verificar as credenciais
+        // Por exemplo, você pode comparar com um nome de usuário e senha pré-definidos
+        val validUsername = "admin"
+        val validPassword = "password"
+        return username == validUsername && password == validPassword
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
