5782b825a89e7b19278e4d85feb99713be782ce8
myrellauchoa <myrellauchoast@gmail.com>
2023-09-18 19:41:47 -0300

    acrescentando base da logica para home e reservas

--- app/src/main/java/com/example/skilloapp/data/CardBookingItem.kt
+++ app/src/main/java/com/example/skilloapp/data/CardBookingItem.kt
@@ -1,9 +1,13 @@
 package com.example.skilloapp.data
 
+import com.example.skilloapp.data.model.reserva.DiaDaSemana
+
 class CardBookingItem(
     var time: String,
     var duration: String,
     var labName: String,
     var subject: String,
-    var room: String
+    var room: String,
+    var id: Int,
+    var diaDaSemana: DiaDaSemana
 )
--- app/src/main/java/com/example/skilloapp/ui/LabBookingActivity.kt
+++ app/src/main/java/com/example/skilloapp/ui/LabBookingActivity.kt
@@ -1,25 +1,34 @@
-package com.example.skilloapp.ui
-
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.skilloapp.R
 import com.example.skilloapp.adapter.CardBookingAdapter
+import com.example.skilloapp.data.ApiService
 import com.example.skilloapp.data.CardBookingItem
+import com.example.skilloapp.data.RetrofitConfig
+import com.example.skilloapp.data.model.reserva.DiaDaSemana
+import com.example.skilloapp.data.model.reserva.SalvarReservaResponse
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.google.android.material.slider.RangeSlider
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LabBookingActivity : AppCompatActivity() {
     private lateinit var cardAdapter: CardBookingAdapter
     private lateinit var cardItemList: MutableList<CardBookingItem>
+    private lateinit var apiService: ApiService // Adicione a instância do ApiService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_horarios)
         supportActionBar?.hide()
+
+        apiService = RetrofitConfig.createService(ApiService::class.java) // Inicialize o ApiService
 
         initializeViews()
         setupRecyclerView()
@@ -30,24 +39,30 @@ class LabBookingActivity : AppCompatActivity() {
     private fun initializeViews() {
         cardItemList = mutableListOf()
         cardAdapter = CardBookingAdapter(cardItemList)
+
+        cardAdapter.setOnReserveClickListener(object : CardBookingAdapter.OnReserveClickListener {
+            override fun onReserveClick(position: Int) {
+                onReserveClick(position)
+            }
+        })
     }
 
     private fun setupRecyclerView() {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Add sample data
+        // Adicione dados de exemplo
         addSampleCardItems()
 
-        // Configure the adapter
+        // Configure o adaptador
         recyclerView.adapter = cardAdapter
     }
 
     private fun addSampleCardItems() {
-        cardItemList.add(CardBookingItem("08:40", "50min", "Laboratório 1", "Matéria 1", "1 DS B"))
-        cardItemList.add(CardBookingItem("09:30", "45min", "Laboratório 2", "Matéria 2", "3 DS B"))
-        cardItemList.add(CardBookingItem("23:30", "45min", "Laboratório 2", "Matéria 2", "3 DS B"))
-        cardItemList.add(CardBookingItem("17:30", "45min", "Laboratório 2", "Matéria 2", "3 DS B"))
+        cardItemList.add(CardBookingItem("08:40", "50min", "Laboratório 1", "Matéria 1", "1 DS B", 1, DiaDaSemana.THURSDAY))
+        cardItemList.add(CardBookingItem("09:30", "45min", "Laboratório 2", "Matéria 2", "3 DS B", 2, DiaDaSemana.WEDNESDAY))
+        cardItemList.add(CardBookingItem("23:30", "45min", "Laboratório 2", "Matéria 2", "3 DS B", 3, DiaDaSemana.TUESDAY))
+        cardItemList.add(CardBookingItem("17:30", "45min", "Laboratório 2", "Matéria 2", "3 DS B", 4, DiaDaSemana.MONDAY))
     }
 
     private fun setupChips() {
@@ -92,5 +107,27 @@ class LabBookingActivity : AppCompatActivity() {
             cardAdapter.cardItemList = filteredReservations
             cardAdapter.notifyDataSetChanged()
         }
+    }
+
+    private fun onReserveClick(position: Int) {
+        val item = cardItemList[position]
+
+        apiService.salvarReserva(item.diaDaSemana, item.id).enqueue(object : Callback<SalvarReservaResponse> {
+            override fun onResponse(call: Call<SalvarReservaResponse>, response: Response<SalvarReservaResponse>) {
+                if (response.isSuccessful) {
+                    showToast("Reserva realizada com sucesso!")
+                } else {
+                    showToast("Erro ao salvar a reserva")
+                }
+            }
+
+            override fun onFailure(call: Call<SalvarReservaResponse>, t: Throwable) {
+                showToast("Erro na requisição da API")
+            }
+        })
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
